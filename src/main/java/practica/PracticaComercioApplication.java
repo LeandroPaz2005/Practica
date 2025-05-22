96b484c8b12f64ce7ade5eb878a47fa190e83b5b
LeandroPazK <U223199603@utp.edu.pe>
2025-05-22 06:41:26 -0500

    Crear la base para seguir practicando Spring Boot

---
+++ src/main/java/practica/PracticaComercioApplication.java
@@ -0,0 +1,13 @@
+package practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practica/PracticaComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
