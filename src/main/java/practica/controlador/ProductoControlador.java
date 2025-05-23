aabe7c768a2846bc8520abcb5535fd2f51635761
LeandroPazK <U223199603@utp.edu.pe>
2025-05-23 13:45:47 -0500

    feat: fucionalidad guardar para producto create y logger para la clase

--- src/main/java/practica/controlador/ProductoControlador.java
+++ src/main/java/practica/controlador/ProductoControlador.java
@@ -1,14 +1,27 @@
 
 package practica.controlador;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import practica.modelo.Producto;
+import practica.modelo.Usuarios;
+import practica.servicio.ProductoServicio;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoControlador {
     
+    private final Logger LOGGER=LoggerFactory.getLogger(ProductoControlador.class);
+    
+    //declar variable para acceder a los metodos
+    @Autowired
+    private ProductoServicio productoServico;
+            
+            
     @GetMapping("")
     public String show(){
     return "productos/show";
@@ -17,5 +30,14 @@ public class ProductoControlador {
     @GetMapping("/create")
     public String create(){
     return "productos/create";
+    }
+    
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este el objeto producto {}", producto);
+        Usuarios u=new Usuarios(1, "", "", "","", "", "", "");
+        producto.setUsuario(u);
+        productoServico.save(producto);
+    return "redirect:/productos";
     }
 }
