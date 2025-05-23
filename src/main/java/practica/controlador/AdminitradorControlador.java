0a27af9f4b3ba716bd5b7b000935c022908a7c9f
LeandroPazK <U223199603@utp.edu.pe>
2025-05-23 07:32:43 -0500

    feat: la clase controlador para el administrador

---
+++ src/main/java/practica/controlador/AdminitradorControlador.java
@@ -0,0 +1,16 @@
+
+package practica.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminitradorControlador {
+    
+    @GetMapping("")
+    public String home(){
+    return "admin/home";
+    }
+}
