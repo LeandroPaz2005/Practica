003dc73a0d53eeaa6874b038c412b7eb14380a20
LeandroPazK <U223199603@utp.edu.pe>
2025-05-29 06:38:05 -0500

    feat: se agrego la funcionalidad y configuracion para que se vea el producto

--- src/main/java/practica/controlador/AdminitradorControlador.java
+++ src/main/java/practica/controlador/AdminitradorControlador.java
@@ -1,16 +1,26 @@
 
 package practica.controlador;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import practica.modelo.Producto;
+import practica.servicio.ProductoServicio;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdminitradorControlador {
     
+    @Autowired
+    private ProductoServicio productoService;
+    
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+        List<Producto> productos=productoService.findAll();
+        model.addAttribute("productos", productos);
     return "administrador/home";
     }
 }
